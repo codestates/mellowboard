bcfe0f9e1b06ab067de870bc4a88c907cc584521
hoony0802 <hoony0802@naver.com>
2021-10-06 18:54:32 +0900

    Update client default settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  body {
+    /* font 설정 */
+    @import url('https://fonts.googleapis.com/css2?family=Gugi&family=Noto+Serif+KR&display=swap');
+    font-family: "Gugi", 'Noto Serif KR', cursive, serif;
+
+    /* 전체 배경화면 설정 */
+      background: linear-gradient(-45deg, #1B1464, #006266, #6F1E51, #cd6133);
+      background-size: 400% 400%;
+      animation: aurora 20s ease infinite;
+      height: 100vh;
+  }
+
+  @keyframes aurora {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="app">Mellow Board!</div>
+      <div className="App">무르익게 익명 게시판!</div>
+    </>
   );
-}
+};
 
 export default App;
