e946d49b91c9fe9dc10d05365417b9508ca4549d
doldolma <hdy3405@gmail.com>
2021-10-06 14:00:15 +0900

    ADD 서버환경변수 파일 추가 및 설정

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use("/", express.static("public"));
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
