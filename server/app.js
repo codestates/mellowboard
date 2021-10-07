e5ff629adeff008c22a5f302c48f19790dbc9d09
doldolma <hdy3405@gmail.com>
2021-10-07 17:41:22 +0900

    FIX 다른경로로 접근해도 react페이지 리턴하도록 수정

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const commentsRouter = require('./routes/comments');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'jade');
 
 app.use(logger('combined'));
@@ -29,20 +29,24 @@ app.use('/comments', commentsRouter);
 // ELB를 위한 헬스체크
 app.get("/healthcheck", (_, res) => res.send("Im alive"));
 
+app.get("*", (req, res) => {
+  return res.sendFile(__dirname + "/public/index.html");
+})
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  return res.sendFile(__dirname + "/public/index.html");
 });
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 const db = require("./models");
